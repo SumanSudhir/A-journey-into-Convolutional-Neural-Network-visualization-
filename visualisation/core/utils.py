e915fb0d0fe11c32d454d851badae6dfb5659547
SumanSudhir <sudhiriitb27@gmail.com>
2021-02-10 17:21:07 +0530

    efficient-net error

--- visualisation/core/utils.py
+++ visualisation/core/utils.py
@@ -91,13 +91,7 @@ def module2traced(module, inputs):
     def trace(module, inputs, outputs):
         modules.append(module)
 
-    # def traverse(module):
-    #     for m in module.children():
-    #         traverse(m)
-    #     is_leaf = len(list(module.children())) == 0
-    #     if is_leaf: handles.append(module.register_forward_hook(trace))
-
-   def traverse(module):
+    def traverse(module):
         if isinstance(module, torch.nn.Conv2d):
             handles.append(module.register_forward_hook(trace))
             return
